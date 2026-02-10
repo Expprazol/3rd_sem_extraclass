b7d62be2b60023da9ff61986f756fdf1065d7046
Expprazol <sapkotaprazol@gmail.com>
2026-02-10 08:29:07 +0545

    php add

---
+++ admin/dashboard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="flex">
+        <div class="w-52 h-screen bg-gray-100">
+            <h2 class="text-center mt-5 font-bold text-2xl">login</h2>
+            <nav class="mt-4">
+                <a href="dashboard.html" class="hover:bg-gray-200 border-b p-3 text-lg block ">dashboard</a>
+                  <a href="categories.html" class="hover:bg-gray-200 border-b p-3 text-lg block ">Categories</a>
+                    <a href="" class="hover:bg-gray-200 border-b p-3 text-lg block ">product</a>
+                      <a href="" class="hover:bg-gray-200 border-b p-3 text-lg block ">Customers</a>
+                        <a href="" class="hover:bg-gray-200 border-b p-3 text-lg block ">orders</a>
+                          <a href="" class="hover:bg-gray-200 border-b p-3 text-lg block ">settings</a>
+                          <a href="" class="hover:bg-gray-200 border-b p-3 text-lg block ">logout</a>
+
+            </nav>
+        </div>
+
+ <div class="p-4 flex-1">
+            <h2 class="font-bold text-2xl">dashboard</h2>
+            <hr class=" h-1 bg-red-600">
+            <p>Rest of the content</p>
+        </div>
+        </div>
+</body>
+</html>
